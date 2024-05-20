e3923548b220d2fa927d23e7d14ac64015947707
Nacho <nacho@MacBook-Air-de-Nacho.local>
2024-05-20 19:21:42 -0300

    cambios en la SSS

---
+++ src/CLASE2.java
@@ -0,0 +1,23 @@
+public class CLASE2 {
+
+
+    //FUNCIONES!!!
+    //y = f(x,w,z,...)
+    //VARIABLE DE SALIDA -> Y (LO QUE ME DEVUELVE)
+    //VARIABLES DE ENTRADA -> x, z, w (LO QUE INGRESO)
+    //FIRMA DE UNA FUNCION -> LO QUE LA HACE UNICA ->   TipoDeAccesoALaFuncion      TipoDeVariableDeSalida      nombreDeLaFuncion (VariablesDeEntrada, ....)
+
+    public static void main(String[] args) {
+        String localidad = obtenerLocalidad("1234");
+    }
+
+
+
+    //FUNCION
+    //TIPO DE ACCESO -> PUBLIC (ACCESO PUBLICO EN CUALQUIER PARTE DEL PROYECTO) / PRIVATE (SOLO ACCEDO DESDE CLASE2) / PROTECTED
+    private static String obtenerLocalidad(String codigoPostal){
+        return "victoria";
+    }
+
+
+}
